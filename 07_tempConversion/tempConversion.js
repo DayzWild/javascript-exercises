991628ac80d309689dc80679c5b982377d92f9e2
Desiree Wildgrube <145672131+DayzWild@users.noreply.github.com>
2024-03-18 11:59:29 +0100

    convertToCelsius convert Fahrenheit temp to Celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  temp -= 32;
+
+  return temp;
+
 };
 
 const convertToFahrenheit = function() {
+
+
+
 };
 
 // Do not edit below this line
